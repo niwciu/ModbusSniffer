e8e3ebb7b3ed4082bd5e4413b89300852b6495f1
Your Name <niwciu@gmail.com>
2025-05-11 20:28:20 +0200

    Readme update + .bat install script created

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Variables
 APP_NAME="ModbusSniffer"
 MAIN_PY="modbus_sniffer_GUI.py"
 ICON="images/icon-4.png"
@@ -7,12 +8,30 @@ DESKTOP_FILE="$APP_NAME.desktop"
 BUILD_DIR="build"
 DIST_DIR="dist"
 LOCAL_APP_DIR="$HOME/.local/share/applications"
+VENV_DIR=".venv"
 
 # Clean previous build
 rm -rf "$BUILD_DIR" "$DIST_DIR" __pycache__ *.spec
 
+# Create virtual environment if it doesn't exist
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv "$VENV_DIR"
+fi
+
+# Activate virtual environment
+source "$VENV_DIR/bin/activate"
+
+# Install dependencies
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+# Install PyInstaller in the virtual environment
+pip install pyinstaller
+
 # Create executable with PyInstaller
-pyinstaller --onefile --name "$APP_NAME" "$MAIN_PY" 
+echo "Creating executable with PyInstaller..."
+pyinstaller --onefile --name "$APP_NAME" "$MAIN_PY"
 
 # Create application directory
 mkdir -p "$BUILD_DIR"
@@ -33,5 +52,9 @@ chmod +x "$BUILD_DIR/$DESKTOP_FILE"
 mkdir -p "$LOCAL_APP_DIR"
 cp "$BUILD_DIR/$DESKTOP_FILE" "$LOCAL_APP_DIR/"
 
-# echo "Application built in $BUILD_DIR/"
-# echo "Shortcut added to menu at: $LOCAL_APP_DIR/$DESKTOP_FILE"
+# Deactivate virtual environment
+deactivate
+
+# Output info
+echo "Application built in $BUILD_DIR/"
+echo "Shortcut added to menu at: $LOCAL_APP_DIR/$DESKTOP_FILE"
